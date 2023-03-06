d16957a681931b0a788844958df5a89983c9825f
Lenny Girardot <l.girardot74@gmail.com>
2023-03-06 21:37:44 +0100

    v1: demo

--- demo/main.js
+++ demo/main.js
@@ -1,11 +1,11 @@
 import { Tutorialjs } from '../dist/index.js'
 
 let tutorial = new Tutorialjs([
-    { element: '#search-input', comment: 'This is a search input', position: 'left' },
-    { element: document.querySelector('#second-card'), comment: 'This is a card', position: 'right' },
-    { element: '#navbar', comment: 'This is a navbar', position: 'sedgsdg' },
-    { element: '#third-card', comment: 'This is another card', position: 'top' },
-    { element: '#first-card', comment: 'This is the first card', position: 'bottom' }
+    { element: '#search-input', comment: 'This is a search input', option: {position: 'bottom', gap: 50} },
+    { element: document.querySelector('#second-card'), comment: 'This is a card', option: {position: 'right'} },
+    { element: '#navbar', comment: 'This is a navbar', option: {position: 'sedgsdg'} },
+    { element: '#third-card', comment: 'This is another card', option: {position: 'top'} },
+    { element: '#first-card', comment: 'This is the first card', option: {position: 'left'} }
 ], {
     nextButtonLabel: 'Get me to the next hint ;)',
     previousButtonLabel: 'Wait what ?',
